6dc2650469f13230ffff9bd6e4256ecd7708fff0
CuongNM <manhcuong.vn.1805@gmail.com>
2021-07-07 12:56:23 +0700

    update

--- server/middleware/handlerCheckPermission.js
+++ server/middleware/handlerCheckPermission.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
     const resources = req.baseUrl + req.route.path;
 
     global.acl.areAnyRolesAllowed(req._user.role[0], resources, req.method.toLowerCase(), function (err, isAllowed) {
-        console.log(err, isAllowed);
+      //  console.log(err, isAllowed);
         if (!isAllowed || err) return res.json({
             code: 403,
             message: MESSAGES.USERNAME_NOT_PERMISSION,
